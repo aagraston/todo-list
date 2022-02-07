e93af32757603f92c5f7512262e931e12c8e8d43
Aaron graston <91895961+aagraston@users.noreply.github.com>
2022-02-07 11:03:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# boiler-plate-1.0
+A very simple boiler-plate for use in tutorial JS projects
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { appendFile } = require('fs');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      inject: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node-modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'],
+      },
+    ],
+  },
+};
